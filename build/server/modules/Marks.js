4d8eb9ea8ca29d9b19739e856155295de0ae4712
Aditya <adityaraj@divami.com>
2023-11-30 14:29:58 +0530

    removed unwanted

--- build/server/modules/Marks.js
+++ build/server/modules/Marks.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+
 const MarksSchema=new mongoose.Schema({
     studentId:{
         type:String,
--- server/Routes/Getusers.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-
-const router=express.Router()
-
-const users=['Adityaraj','Karn Bhati','Sadhvi Sahu','Shivani Sharma','Nidhi Patel','Garvit']
-
-
-router.get('/',(req,res))
